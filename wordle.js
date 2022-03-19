9a675cdecd6e255192615fde3ce4e02e1050aa05
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-03-20 00:01:28 +1100

    saving changes : printing word in first row

--- wordle.js
+++ wordle.js
@@ -1,2 +1,25 @@
-const wordOfTheDay = "Querty"
- const inputText = document.getElementById
+const wordOfTheDay = "happy"
+let attemptCounter = 0;
+function display() {
+    attemptCounter  = attemptCounter + 1;
+    const userinput = document.getElementById('input').value.toUpperCase();
+    // console.log(userinput);
+    //splitting each letter in the word
+    const newArrayOfletters = userinput.split("");
+    // console.log(newArrayOfletters);
+
+    const parentDivs = document.querySelectorAll(".row");
+    console.log(parentDivs);
+
+    const row1Divs = document.querySelector("#row-1").children;
+    //console.log(row1Divs);
+         for (let index in row1Divs){
+           let letter = newArrayOfletters[index];
+        //    console.log(letter);
+           let rowDiv = row1Divs[index]; 
+        //    console.log(rowDiv) ;
+           rowDiv.textContent = letter ;
+         }
+    
+
+}
