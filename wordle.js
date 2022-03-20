91612a6f74348dda12522782839dc1755c6780d1
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-03-20 11:32:14 +1100

    commiting -changing the box color wrd to letter position

--- wordle.js
+++ wordle.js
@@ -1,25 +1,42 @@
-const wordOfTheDay = "happy"
+const wordOfTheDay = "hoppy"
+const arrayOFWordOfTheDay = wordOfTheDay.toUpperCase();
+console.log(arrayOFWordOfTheDay);
+
 let attemptCounter = 0;
 function display() {
     attemptCounter  = attemptCounter + 1;
     const userinput = document.getElementById('input').value.toUpperCase();
-    // console.log(userinput);
     //splitting each letter in the word
     const newArrayOfletters = userinput.split("");
     // console.log(newArrayOfletters);
-
-    const parentDivs = document.querySelectorAll(".row");
-    console.log(parentDivs);
-
-    const row1Divs = document.querySelector("#row-1").children;
-    //console.log(row1Divs);
-         for (let index in row1Divs){
-           let letter = newArrayOfletters[index];
-        //    console.log(letter);
-           let rowDiv = row1Divs[index]; 
-        //    console.log(rowDiv) ;
-           rowDiv.textContent = letter ;
-         }
-    
-
+    if(attemptCounter < 6){
+    //to put the word in 1st row in Attempt 1
+    const rowDivs = document.querySelector("#row-"+`${attemptCounter}`).children;
+    console.log(rowDivs);
+        for(let i=0; i < rowDivs.length; i++){
+            rowDivs[i].textContent = newArrayOfletters[i];
+            if(arrayOFWordOfTheDay.includes(newArrayOfletters[i])){
+                if(arrayOFWordOfTheDay[i]==newArrayOfletters[i]){
+                    rowDivs[i].style.backgroundColor = "green";
+                }else{
+                console.log("letter  in word");
+                rowDivs[i].style.backgroundColor = "orange";
+            }
+                
+            }
+                 else{
+                    rowDivs[i].style.backgroundColor = "gray";
+                    }
+                }
+    }else{
+        alert("You have Exhausted your not of tries")
+    }   
 }
+ //Examples:   
+// The green mean letter the correct spot.
+
+    
+// The letter I is in the word but in the wrong spot.
+
+    
+// The letter U is not in the word in any spot.
